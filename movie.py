6cde606698974a216e5b9e84a40bdda20fec827b
candyolivia <candyoliviamawalim@gmail.com>
2016-12-12 17:13:58 +0700

    convert to class movie

--- movie.py
+++ movie.py
@@ -1,70 +1,72 @@
 import re, string
 import csv
 
-movies = []
-favoriteActors = {}
-favoriteDirectors = {}
-favoriteMovies = [1,2,3,4,19,27,34]
+class Movie:
+	def __init__(self):
+		self.movies = []
+		self.favoriteActors = {}
+		self.favoriteDirectors = {}
+		self.favoriteMovies = [1,2,3,4,19,27,34]
 
-actorIdx = [10, 6, 14]
-directorIdx = 1
+		self.actorIdx = [10, 6, 14]
+		self.directorIdx = 1
 
-def init():
-	with open("movie_metadata.csv", 'rb') as f:
-		global movies
-		movies = list(csv.reader(f, delimiter=','))
+	def init(self):
+		with open("movie_metadata.csv", 'rb') as f:
+			self.movies = list(csv.reader(f, delimiter=','))
 
-def getFavoriteActors():
-	actors = countActors()
-	numFavoriteMovies = len(favoriteMovies)
-	for i in range(numFavoriteMovies):
-		movieIdx = favoriteMovies[i]
-		numActors = len(actorIdx)
-		for j in range(numActors):
-			actor = movies[movieIdx][actorIdx[j]]
-			if favoriteActors.has_key(actor):
-				favoriteActors[actor] += 1
+	def getFavoriteActors(self):
+		actors = self.countActors()
+		numFavoriteMovies = len(self.favoriteMovies)
+		for i in range(numFavoriteMovies):
+			movieIdx = self.favoriteMovies[i]
+			numActors = len(self.actorIdx)
+			for j in range(numActors):
+				actor = self.movies[movieIdx][self.actorIdx[j]]
+				if self.favoriteActors.has_key(actor):
+					self.favoriteActors[actor] += 1
+				else:
+					self.favoriteActors[actor] = 1
+
+			for j in range(numActors):
+				actor = self.movies[movieIdx][self.actorIdx[j]]
+				# print actor, actors[actor]
+				if actors[actor] < 3:
+					del self.favoriteActors[actor]
+
+	def countActors(self):
+		actors = {}
+		numMovies = len(self.movies) - 1
+		for i in range(numMovies):
+			numActors = len(self.actorIdx)
+			for j in range(numActors):
+				actor = self.movies[i+1][self.actorIdx[j]]
+				if actors.has_key(actor):
+					actors[actor] += 1
+				else:
+					actors[actor] = 1
+		return actors
+
+	def getFavoriteDirectors(self):
+		numFavoriteMovies = len(self.favoriteMovies)
+		for i in range(numFavoriteMovies):
+			movieIdx = self.favoriteMovies[i]
+			director = self.movies[movieIdx][self.directorIdx]
+			if self.favoriteDirectors.has_key(director):
+				self.favoriteDirectors[director] += 1
 			else:
-				favoriteActors[actor] = 1
+				self.favoriteDirectors[director] = 1
 
-		for j in range(numActors):
-			actor = movies[movieIdx][actorIdx[j]]
-			# print actor, actors[actor]
-			if actors[actor] < 3:
-				del favoriteActors[actor]
-
-def countActors():
-	actors = {}
-	numMovies = len(movies) - 1
-	for i in range(numMovies):
-		numActors = len(actorIdx)
-		for j in range(numActors):
-			actor = movies[i+1][actorIdx[j]]
-			if actors.has_key(actor):
-				actors[actor] += 1
-			else:
-				actors[actor] = 1
-	return actors
-
-def getFavoriteDirectors():
-	numFavoriteMovies = len(favoriteMovies)
-	for i in range(numFavoriteMovies):
-		movieIdx = favoriteMovies[i]
-		director = movies[movieIdx][directorIdx]
-		if favoriteDirectors.has_key(director):
-			favoriteDirectors[director] += 1
-		else:
-			favoriteDirectors[director] = 1
-
-def countWeight(movieIdx):
-	pass
+	def countWeight(movieIdx):
+		pass
 
 def main():
-	init()
-	getFavoriteActors()
-	print favoriteActors
-	getFavoriteDirectors()
-	print favoriteDirectors
+	movie = Movie()
+	movie.init()
+	movie.getFavoriteActors()
+	print movie.favoriteActors
+	movie.getFavoriteDirectors()
+	print movie.favoriteDirectors
 
 if __name__ == "__main__":
     main()
