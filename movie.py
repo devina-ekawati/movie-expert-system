608989efb95359d9ecadb072fa6bceacb223fa44
angelynz95 <angelynz95>
2016-12-12 16:11:19 +0700

    create getFavoriteActors

--- movie.py
+++ movie.py
@@ -2,10 +2,29 @@ import re, string
 import csv
 
 movies = []
+favoriteActors = {}
+favoriteMovies = []
+
+actorIdx = [10, 6, 14]
 
 def init():
 	with open("movie_metadata.csv", 'rb') as f:
 		movies = csv.reader(f, delimiter=',')
+
+def getFavoriteActors():
+	numFavoriteMovies = len(favoriteMovies)
+	for i in range(favoriteMovies):
+		movieIdx = movies[i]
+		numActors = len(actorIdx)
+		for j in range(numActors):
+			actor = movies[movieIdx[actorIdx[j]]]
+			if favoriteActors.has_key(actor):
+				favoriteActors[actor] += 1
+			else:
+				favoriteActors[actor] = 1
+
+def countWeight(movieIdx):
+	pass
 
 def main():
 	init()
